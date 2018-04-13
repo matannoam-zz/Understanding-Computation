2bf5a5318c32a674a1975b96563b530f2ccfe615
Matan Noam Shavit <matannoam@gmail.com>
2018-04-13 19:55:23 -0400

    add representation to add expression, abstract inclusion of gillomets

--- chapter2/small_step.py
+++ chapter2/small_step.py
@@ -1,7 +1,27 @@
-class Number:
+class ExpressionMixin:
+    def __repr__(self):
+        return f'«{self.clean_repr}»'
+
+
+class Number(ExpressionMixin):
 
     def __init__(self, value):
         self.value = value
 
     def is_reducible(self):
         return False
+
+    @property
+    def clean_repr(self):
+        return self.value
+
+
+class Add(ExpressionMixin):
+
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    @property
+    def clean_repr(self):
+        return f'{self.left.clean_repr} + {self.right.clean_repr}'
--- chapter2/test_small_step.py
+++ chapter2/test_small_step.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from nose.tools import eq_
 
-from small_step import Number
+from small_step import Number, Add
 
 
 class NumberTests(TestCase):
@@ -13,3 +13,18 @@ class NumberTests(TestCase):
     def test_stores_value(self):
         number = Number(1)
         eq_(number.value, 1)
+
+    def test_repr(self):
+        number = Number(1)
+        eq_(str(number), '«1»')
+
+
+class AddTests(TestCase):
+
+    def test_repr(self):
+        number1 = Number(1)
+        number2 = Number(2)
+
+        add = Add(number1, number2)
+
+        eq_(str(add), '«1 + 2»')
